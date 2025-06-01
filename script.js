2ee3ca116236f6cc49442f5bc8b6131b5846a3ba
Vaidehi <vaidehiraut21@gmail.com>
2025-06-01 14:07:36 -0700

    Commit chnages

--- script.js
+++ script.js
@@ -1,61 +1,105 @@
 const input = document.querySelector("#input");
-const serachBtn = document.querySelector("#search-btn");
+const searchBtn = document.querySelector("#search-btn");
 const result = document.querySelector(".result");
 const body = document.querySelector("body");
-const header = document.querySelector(".header");
-serachBtn.addEventListener("click", () => {
-  if (input.value === "") {
+const IMG_BASE = "https://image.tmdb.org/t/p/w500";
+
+searchBtn.addEventListener("click", () => {
+  const query = input.value.trim();
+  if (query === "") {
     result.innerHTML = `<h3 id="msg">Enter a movie name</h3>`;
+    return;
   }
-  fetch(`https://omdbapi.com/?t=${input.value}&apikey=41c5891e`)
-    .then((response) => {
-      return response.json();
-    })
+
+  result.innerHTML = "";
+  body.style.backgroundImage = "";
+  body.classList.remove("blur-bg");
+  
+
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${input.value}&api_key=c12b82f343bdde64c6673d9e043c63ba`
+  )
+    .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      result.innerHTML = `
-        <div class="movie-info">
-  <img src="${data.Poster}" alt="Movie Poster">
+      if (!data.results.length) {
+        result.innerHTML = `<h3 id="msg">Movie not found</h3>`;
+        return;
+      }
 
-  <div class="title">
-    <h4 id="movie-title">${data.Title}</h4>
-
-    <div class="rating">
-      <i id="star" class="fa-solid fa-star"></i>
-      <p id="rate">${data.imdbRating}</p>
-    </div>
-
-    <div class="extra-info">
-      <p>${data.Rated}</p>
-      <p>${data.Year}</p>
-      <p>${data.Runtime}</p>
-    </div>
-
-    <div class="genre-section">
-      <p class="genre">${data.Genre.split(",")[0]}</p>
-      <p class="genre">${data.Genre.split(",")[1]}</p>
-      <p class="genre">${data.Genre.split(",")[2]}</p>
+      const movie =
+        data.results.find(
+          (m) => m.title.toLowerCase() === query.toLowerCase()
+        ) || data.results[0];
+      console.log(movie.title);
+      fetch(
+        `https://api.themoviedb.org/3/movie/${movie.id}?api_key=c12b82f343bdde64c6673d9e043c63ba`
+      )
+        .then((res) => res.json())
+        .then((details) => {
+          let genreHtml = "";
+          details.genres.slice(0, 3).forEach((genre) => {
+            genreHtml += `<p class="genre">${genre.name}</p>`;
+          });
+          console.log(details.runtime);
+          result.innerHTML = `
+  <div class="movie-info">
+    <img src="${
+      movie.poster_path ? IMG_BASE + movie.poster_path : ""
+    }" alt="Movie Poster">
+    <div class="title">
+      <h4 id="movie-title">${movie.title}</h4>
+      <div class="rating">
+        <i id="star" class="fa-solid fa-star"></i>
+        <p id="rate">${movie.vote_average}</p>
+      </div>
+      <div class="extra-info">
+        <p>${movie.original_language}</p>
+        <p>${movie.release_date}</p>
+        <p>${details.runtime} mins</p>
+      </div>
+      <div class="genre-section">
+        ${genreHtml}
+      </div>
     </div>
   </div>
-</div>
-<div class="info-section">
-<div class="plot-section">
-  <h4>Plot</h4>
-  <p id="plot">${data.Plot}</p>
-</div>
+  <div class="info-section">
+    <div class="plot-section">
+      <h4>Plot</h4>
+      <p id="plot">${movie.overview}</p>
+    </div>
+  </div>
+`;
 
-<div class="cast-section">
-  <h4>Cast</h4>
-  <p id="cast">${data.Actors}</p>
-</div>
-<div class="director-section">
-  <h4>Director</h4>
-  <p id="director">${data.Director}</p> 
-</div>
-</div>
-        `;
-      body.style.backgroundImage = `url(${data.Poster})`;
-      body.classList.add("blur-bg");
-      header.innerHTML = "";
+          fetch(
+            `https://api.themoviedb.org/3/movie/${movie.id}/videos?language=en-US&api_key=c12b82f343bdde64c6673d9e043c63ba`
+          )
+            .then((res) => res.json())
+            .then((videoData) => {
+              const trailer = videoData.results.find(
+                (v) => v.type == "Trailer" && v.site === "YouTube"
+              );
+              if (trailer) {
+                result.innerHTML += `
+  <div class="trailer-section">
+    <h4>Trailer</h4>
+    <iframe width="100%" height="400" src="https://www.youtube.com/embed/${trailer.key}" frameborder="0" allowfullscreen></iframe>
+  </div>`;
+              } else {
+                result.innerHTML += `<p>No trailer available.</p>`;
+              }
+
+              body.style.backgroundImage = `url(${
+                movie.poster_path ? IMG_BASE + movie.poster_path : ""
+              })`;
+              body.classList.add("blur-bg");
+              const headerElement = document.querySelector(".header");
+              if (headerElement) {
+                headerElement.remove();
+              }
+            });
+        });
+    })
+    .catch(() => {
+      result.innerHTML = `<h3 id="msg">Error fetching data</h3>`;
     });
 });
